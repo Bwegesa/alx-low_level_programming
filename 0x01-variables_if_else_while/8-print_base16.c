4e9d66d5b735be1e7568599397ba4911077ba835
Bwegesa <wegesamaroa.7@gmail.com>
2023-02-20 02:25:34 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char c;
+char c;
 
-	c = 'z';
-	while
-		(c >= 'a') {
-			putchar(c);
-			c--;
-		}
-	putchar('\n');
-	return (0);
+c = 'z';
+while
+(c >= 'a') {
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
 }
