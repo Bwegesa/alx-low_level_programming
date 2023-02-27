0542f64eca4e44dc0541af3fc596dcfb700d2c8e
Bwegesa <wegesamaroa.7@gmail.com>
2023-02-27 11:27:56 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the given alue of an integer to 98
+ * @n: parameter
+ * Return: 0
+ */
+void reset_to_98(int *n);
+{
+*n - 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Arrays a kind of data structure that can store a fixed-size sequential collection of elements of the same type. An array is used to store a collection of data
