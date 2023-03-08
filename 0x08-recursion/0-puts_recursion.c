d9047221e816b49868078e7dbdc96452248cc782
Bwegesa <wegesamaroa.7@gmail.com>
2023-03-08 09:23:58 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - _puts_recursion that prints a string, followed by a new line.
- * @*s - the string to be printed
+ * _puts_recursion - that prints a string, followed by a new line.
+ * @*s: the string to be printed
  * Return: always 0 (success)
  * On error, -1 is returned, and errno is set appropriately
  */
